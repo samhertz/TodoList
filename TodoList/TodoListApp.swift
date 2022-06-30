2b932fc3e5edaf646614ade746b70ed004461cb8
Sam Hertz <samhertz@vt.edu>
2022-06-30 10:35:46 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Samuel Hertz on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
