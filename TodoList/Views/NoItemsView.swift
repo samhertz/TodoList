37a8d51c783e5c1db902755095ca4d51c43c9d6a
Sam Hertz <samhertz@vt.edu>
2022-07-01 08:48:15 -0700

    - Used animations to improve user experience - Added NoItemsView that appears if there are no todo items in list

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,60 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Samuel Hertz on 6/30/22.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                NavigationLink {
+                    AddView()
+                } label: {
+                    Text("Add Something 🥳")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                }
+
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(.easeInOut) {
+                animate.toggle()
+            }
+        }
+        
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        NavigationView {
+            NoItemsView()
+                .navigationTitle("Title")
+        }
+    }
+}
